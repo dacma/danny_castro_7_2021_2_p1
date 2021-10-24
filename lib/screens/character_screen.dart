4a83985d51fb0e257b2ccb91f11f35090dd2e1fd
dacma <dannycastro42459@correo.itm.edu.co>
2021-10-23 20:26:47 -0500

    principal menu implemented

--- lib/screens/character_screen.dart
+++ lib/screens/character_screen.dart
@@ -1,8 +1,8 @@
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+//import 'package:http/http.dart' as http;
 
-import 'package:rickmorty_app/helpers/constans.dart';
+//import 'package:rickmorty_app/helpers/constans.dart';
 
 class CharacterScreen extends StatefulWidget {
   const CharacterScreen({ Key? key }) : super(key: key);
@@ -14,49 +14,43 @@ class CharacterScreen extends StatefulWidget {
 class _CharacterScreenState extends State<CharacterScreen> {
   @override
   Widget build(BuildContext context) {
-
-
-
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-              _showButtons(),
-          ],
-        )
-      ),
-    );
-  }
-
-  Widget _showButtons() {
-    return Container(
-      margin: EdgeInsets.only(left: 10, right: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-
-      children:<Widget> [
-        Expanded(
-        child: ElevatedButton(
-          child: Text('Personajes'),
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.resolveWith<Color>(
-              (Set<MaterialState> states) {
-                return Color(0xFF120E43);
-              }
-            ),
-          ),
-          onPressed: () => _charterInformation(), 
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              _showLogo(),
+              _showButton(),
+            ],
         ),
+
       ),
-      ],
-    ),
+
     );
   }
 
-  void _charterInformation() async {
-  var url = Uri.parse('${Constans.apiUrl}/api/character');
 
+
+
+  Widget _showLogo() {
+    return Image(
+    image: AssetImage('assets/rick_morty.png'),
+    width: 200,
+    );
+  }
+
+  Widget _showButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget> [
+         ElevatedButton(
+           child: Text('Mostrar personajes'),
+           onPressed: (){}, 
+           
+           ), 
+      ],
+
+    );
 
   }
 
